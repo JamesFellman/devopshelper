5f5287c256188d39dc47dfc980e0e3b9ee74d5bc
jdfellman <fellmanjames@gmail.com>
2017-08-31 21:12:34 -0500

    added setup with logback

--- src/test/java/com/devopshelper/DevopshelperApplicationTests.java
+++ src/test/java/com/devopshelper/DevopshelperApplicationTests.java
@@ -5,11 +5,18 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = DevopshelperApplication.class)
 @SpringBootTest
+@WebAppConfiguration
+
 public class DevopshelperApplicationTests {
 
 
@@ -18,10 +25,10 @@ public class DevopshelperApplicationTests {
 
 	@Test
 	public void testMessageByLocalService() throws Exception {
-		String expectpedResult = "Bootstrap starter template";
+		String expectedResult = "Bootstrap starter template";
 		String messageId = "index.main.callout";
 		String actual = i18NService.getMessage(messageId);
-		Assert.assertEquals("The actual and expected String don't match", expectpedResult, actual);
+		Assert.assertEquals("The actual and expected String don't match", expectedResult, actual);
 	}
 
 }
